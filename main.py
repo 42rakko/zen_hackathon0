91103f4b7719f6e15155ec653f209c396e6f600e
mewubuntu <rakkotan@gmail.com>
2025-09-26 01:15:56 +0900

    20250926011556

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import List, Tuple
 import copy
-from local_driver import Alg3D, Board # ローカル検証用
-# from framework import Alg3D, Board # 本番用
+# from local_driver import Alg3D, Board # ローカル検証用
+from framework import Alg3D, Board # 本番用
 
 size = 4
 
