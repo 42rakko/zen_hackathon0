15f386593db4236e96f9a0fbad103bed33629a34
mewubuntu <rakkotan@gmail.com>
2025-09-26 15:15:01 +0900

    20250926151500

--- main.py
+++ main.py
@@ -130,20 +130,21 @@ class MyAI(Alg3D):
                 best_score = -9999
                 best = None
                 for nextmove in nextmoves:
-                    new_selfboard = self.simulate_move(board, nextmove, player) #相手が置いたときの盤面
-                    score_self = self.evaluate_board(new_selfboard, player, nextmove, 0.5, round + 1, oppflag)
-                    new_opponentboard = self.simulate_move(board, nextmove, 3 - player) #相手が置いたときの盤面
-                    score_opponent = self.evaluate_board(new_opponentboard, 3 - player, nextmove, 0.8, round + 1, 1 - oppflag)
+                    if oppflag == 0:
+                        new_selfboard = self.simulate_move(board, nextmove, player) #相手が置いたときの盤面
+                        score_self = self.evaluate_board(new_selfboard, player, nextmove, 0.5, round + 1, oppflag)
+                        new_opponentboard = self.simulate_move(board, nextmove, 3 - player) #相手が置いたときの盤面
+                        score_opponent = self.evaluate_board(new_opponentboard, 3 - player, nextmove, 0.8, round + 1, 1 - oppflag)
 
-                    if (oppflag == 0 and score_opponent > 10000000):
-                        return -300000000
+                        if (oppflag == 0 and score_opponent > 10000000):
+                            return -300000000
                 
 
-                    if score_self > best_score:
-                        best_score = score_self
-                    if score_opponent > best_score:
-                        best_score = score_opponent
-                score += best_score
+                #     if score_self > best_score:
+                #         best_score = score_self
+                #     if score_opponent > best_score:
+                #         best_score = score_opponent
+                # score += best_score
                                 
         return score
 
