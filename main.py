863e1c65b704c3c56b996064cb7ee790aec978a7
mewubuntu <rakkotan@gmail.com>
2025-09-26 14:01:29 +0900

    20250926140129

--- main.py
+++ main.py
@@ -130,17 +130,16 @@ class MyAI(Alg3D):
                 best_score = -9999
                 best = None
                 for nextmove in nextmoves:
-                    new_selfboard = self.simulate_move(board, nextmove, player) #相手が置いたときの盤面
-                    score_self = self.evaluate_board(new_selfboard, player, nextmove, 0.5, round + 1, oppflag)
-                    score_opponent = -100000
                     if oppflag == 0:
+                        new_selfboard = self.simulate_move(board, nextmove, player) #相手が置いたときの盤面
+                        score_self = self.evaluate_board(new_selfboard, player, nextmove, 0.5, round + 1, oppflag)
                         new_opponentboard = self.simulate_move(board, nextmove, 3 - player) #相手が置いたときの盤面
                         score_opponent = self.evaluate_board(new_opponentboard, 3 - player, nextmove, 0.5, round + 1, 1 - oppflag)
                     
-                    if score_self > best_score:
-                        best_score = score_self
-                    if score_opponent > best_score:
-                        best_score = score_opponent
+                        if score_self > best_score:
+                            best_score = score_self
+                        if score_opponent > best_score:
+                            best_score = score_opponent
                 score += best_score
 
 
