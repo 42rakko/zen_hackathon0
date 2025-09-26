7d0129fd4ffd34e0559e85ea1372088c2255adaf
mewubuntu <rakkotan@gmail.com>
2025-09-26 15:18:56 +0900

    20250926151856

--- main.py
+++ main.py
@@ -131,12 +131,12 @@ class MyAI(Alg3D):
                 best = None
                 for nextmove in nextmoves:
                     if oppflag == 0:
-                        new_selfboard = self.simulate_move(board, nextmove, player) #相手が置いたときの盤面
-                        score_self = self.evaluate_board(new_selfboard, player, nextmove, 0.5, round + 1, oppflag)
+                        # new_selfboard = self.simulate_move(board, nextmove, player) #相手が置いたときの盤面
+                        # score_self = self.evaluate_board(new_selfboard, player, nextmove, 0.5, round + 1, oppflag)
                         new_opponentboard = self.simulate_move(board, nextmove, 3 - player) #相手が置いたときの盤面
                         score_opponent = self.evaluate_board(new_opponentboard, 3 - player, nextmove, 0.8, round + 1, 1 - oppflag)
 
-                        if (oppflag == 0 and score_opponent > 10000000):
+                        if score_opponent > 100000000:
                             return -300000000
                 
 
