431cce7a72ce21d99d2101eb18c0ab0b7e613bc6
mewubuntu <rakkotan@gmail.com>
2025-09-26 14:38:06 +0900

    20250926143806

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ class MyAI(Alg3D):
                     new_selfboard = self.simulate_move(board, nextmove, player) #相手が置いたときの盤面
                     score_self = self.evaluate_board(new_selfboard, player, nextmove, 0.5, round + 1, oppflag)
                     new_opponentboard = self.simulate_move(board, nextmove, 3 - player) #相手が置いたときの盤面
-                    score_opponent = self.evaluate_board(new_opponentboard, 3 - player, nextmove, 0.5, round + 1, 1 - oppflag)
+                    score_opponent = self.evaluate_board(new_opponentboard, 3 - player, nextmove, 1, round + 1, 1 - oppflag)
                     
                     if score_self > best_score:
                         best_score = score_self
