e13b8528028be2da5f2f363c9113b4134aaa5c59
42rakko <rakkotan@gmail.com>
2025-09-26 09:59:36 +0900

    20250926095936

--- main.py
+++ main.py
@@ -93,19 +93,20 @@ class MyAI(Alg3D):
             elif zs >= 1:
                 score += 0
             
+            #列の向き
             if zt == 1:
-                score += 100
+                score += 300
             elif zt == 2:
-                score += 50
+                score += 100
             elif zt == 3:
                 score += 10
 
             if x == 0 or x == 3:
-                score += 10
-            elif x == 1 or x == 2:
                 score += 30
+            elif x == 1 or x == 2:
+                score += 100
             if y == 0 or y == 3:
-                score += 10
+                score += 100
             elif y == 1 or y == 2:
                 score += 30
 
